19c82ead5507f3f56c1a99947973da1caa69e4e0
github username <github email address>
2020-01-28 14:39:58 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,23 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get "/" do
+      "Welcome to the Nested Forms Lab!"
+    end 
+
+    get "/new" do 
+    #  "hello world"
+      erb :"pirates/new"
+    end 
+    
+    post "/pirates" do 
+      @pirate = Pirate.new(params)
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end 
+      @ships = Ship.all
+      erb :"pirates/show"
+    end 
 
   end
 end
