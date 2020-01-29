47c99942abbf1c7e85aeaab7baa98f40331030e2
Lindsay Mecher <lindsaymecher@gmail.com>
2020-01-29 17:28:44 -0600

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(params)
+    # binding.pry
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    self.all.clear
+  end
+
+
 end
